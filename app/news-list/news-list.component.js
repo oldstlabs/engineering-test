8bc3419e73f0e3a9e29e9964e00a0b7cc88b9810
Vizibl Developer <developers@vizibl.co>
2019-02-12 11:15:15 +0000

    Implement ordering feature

--- app/news-list/news-list.component.js
+++ app/news-list/news-list.component.js
@@ -6,11 +6,15 @@ angular.
     templateUrl: 'news-list/news-list.template.html',
     controller: ['Lucyd',
       function NewsListController(Lucyd) {
+        this.orderBy = 'title';
 
         this.onChange = function() {
           if (this.term) {
              Lucyd.query({term: this.term}, function (response) {
               this.items = response.payload[0];
+              this.items.sort(function (a, b) {
+                return (a[this.orderBy] < b[this.orderBy]) ? -1 : (a[this.orderBy] > b[this.orderBy]) ? 1 : 0;
+              }.bind(this));
             }.bind(this));
           }
         }
